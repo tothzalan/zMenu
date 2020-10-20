b6a0f9bc3ae197a9ae91c02d21cf0b844b1d6616
Zalan <toth.zalan03@gmail.com>
2020-10-20 16:02:11 +0200

    Added menu system

--- main.cpp
+++ main.cpp
@@ -13,25 +13,63 @@ std::vector<std::string> get_files(std::string path) {
 	return files;
 }
 
-int main(){
-	std::string path = "/home/user/bin/zos/";
-	std::vector<std::string> files = get_files(path);
-	int index = -1;
+int main() {  
+    std::string path = "/bin/zos/";
+	std::vector<std::string> list = get_files(path);
+    WINDOW *w;
+    char item[list.size()];
+    int ch, i = 0;
+    bool canbreak = false;
+ 
     initscr();
-	for(int i = 0; i < files.size(); i++) {
-		printw("%s\n", files[i].c_str());
-		refresh();
-	}
-	while(index < 0 || index > files.size()) {
-		printw("Which file to open? (0 - %d) ", files.size() - 1);
-		refresh();
-		char input[32];
-		getstr(input);
-		index = std::stoi(input);
-	}
-	printw("You opened: %s", (path + files[index]).c_str());
-	refresh();
+
+    int x,y;
+    getmaxyx(stdscr, x, y);
+
+    w = newwin( x-1, y-1, 1, 1 );
+    box( w, 0, 0 );
+     
+    for( i=0; i<list.size(); i++ ) {
+        if( i == 0 ) 
+            wattron( w, A_STANDOUT );
+        else
+            wattroff( w, A_STANDOUT );
+        sprintf(item, "%s",  list[i].c_str());
+        mvwprintw( w, i+1, 2, "%s", item );
+    }
+ 
+    wrefresh( w );
+ 
+    i = 0;
+    noecho();
+    keypad( w, TRUE );
+    curs_set( 0 );
+
+    while(( ch = wgetch(w)) != 'q' && !canbreak){ 
+         
+            sprintf(item, "%s",  list[i].c_str()); 
+            mvwprintw( w, i+1, 2, "%s", item ); 
+            switch( ch ) {
+                case KEY_UP:
+                    i--;
+                    i = ( i < 0 ) ? list.size()-1 : i;
+                    break;
+                case KEY_DOWN:
+                    i++;
+                    i = ( i > list.size()-1 ) ? 0 : i;
+                    break;
+                case KEY_RIGHT:
+                    canbreak = true;
+                    break;
+            }
+                wattron( w, A_STANDOUT );
+
+                sprintf(item, "%s",  list[i].c_str());
+                mvwprintw( w, i+1, 2, "%s", item);
+
+                wattroff( w, A_STANDOUT );
+    }
+    delwin( w );
     endwin();
-	system((path + files[index]).c_str());
-    return 0;
+    system((path + list[i]).c_str());
 }
